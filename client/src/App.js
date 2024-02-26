5bc73324b64ec41983b430bd0460d63c510deedf
majdeddinea <aboaljodm89@@gmail.com>
2024-02-26 16:44:50 +0100

    Integrate client directory directly into main project

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="bg-red-500 text-white p-6">
+      <div className="container mx-auto">
+        <h1 className="text-xl font-semibold">To Do List</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
