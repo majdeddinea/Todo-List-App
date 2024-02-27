854ace00971e964329222a6d40501c0b58b2ff4e
majdeddinea <aboaljodm89@@gmail.com>
2024-02-27 23:18:28 +0100

    Edit the update popup width for mobile version

--- client/src/components/Updatetask.js
+++ client/src/components/Updatetask.js
@@ -42,18 +42,21 @@ function Updatetask(props) {
 
   return (
     <div
-      className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full"
+      className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center px-4"
       id="my-modal"
     >
-      <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
-        <div className="mt-3 text-center">
+      <div
+        className="relative p-5 border shadow-lg rounded-md bg-white mx-2"
+        style={{ maxWidth: "96%", width: "350px" }}
+      >
+        <div className="text-center">
           <h3 className="text-lg leading-6 font-medium text-gray-900">
             Update Task
           </h3>
           <form className="mt-2 px-6 py-4">
             <input
               type="text"
-              className="mt-2 mb-2 appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              className="mt-2 mb-2 appearance-none block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               placeholder="Update Task . . ."
               value={task}
               onChange={(e) => setTask(e.target.value)}
